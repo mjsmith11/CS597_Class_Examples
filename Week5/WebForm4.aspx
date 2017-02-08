24a0b5bc4fa38f684ad8fbcc0fdeff72f0de6335
Matt Smith <Matt.smith@ontariosystems.com>
2017-02-08 14:50:28 -0500

    Cryptography and SQL databases

---
+++ Week5/WebForm4.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm4.aspx.cs" Inherits="Week5.WebForm4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lblUsername" runat="server" Text="Username"></asp:Label>
+        &nbsp;&nbsp;
+        <asp:TextBox ID="tbxUsername" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblPassword" runat="server" Text="Password"></asp:Label>
+        &nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="tbxPassword" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Week5/WebForm4.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Week5
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = tbxUsername.Text;
+            string plainTextPassword = tbxPassword.Text;
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(plainTextPassword));
+            sha1.ComputeHash(ASCIIEncoding.ASCII.GetBytes(plainTextPassword));
+            byte[] result = sha1.Hash;
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i<result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2")); //x2 is for 2 hex because we are sending in a byte at a time
+            }
+            Response.Write(strBuilder.ToString());
+        }
+    }
+}
