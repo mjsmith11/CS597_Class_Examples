afd3ab28cbdecf822a2e406b530bd0d7fe2df2e4
Matt Smith <mjsmith92@outlook.com>
2017-02-15 11:35:27 -0800

    Queries Class

---
+++ Week6/Queries.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Week6
+{
+    public class Queries
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+
+        public Queries()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Week6CS"].ConnectionString);
+        }
+
+        public void Connect()
+        {
+            conn.Open();
+        }
+
+        public void Disconnect()
+        {
+            conn.Close();
+        }
+
+        public SqlCommand GetSqlCommand()
+        {
+            return cmd;
+        }
+
+        public void AddQueryParameter(string parameterName, object parameterValue)
+        {
+            cmd.Parameters.AddWithValue(parameterName, parameterValue);
+        }
+        public void CreateSqlCommand(string qry)
+        {
+            cmd = new SqlCommand(qry, conn);
+        }
+
+        public DataTable RunSelectDataAdapterQuery()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
---
+++ Week6/WebForm2.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Week6
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Queries qry = new Queries();
+            //qry.CreateSqlCommand("SELECT * FROM EMPLOYEES");
+            //gvDisplay.DataSource = qry.RunSelectDataAdapterQuery();
+            //gvDisplay.DataBind();
+            Queries qry = new Queries();
+            qry.CreateSqlCommand("Update Employees Set Salary=@s WHERE EmployeeId=@e");
+            qry.AddQueryParameter("@s", 90000);
+            qry.AddQueryParameter("@e", 3);
+            qry.Connect();
+            qry.GetSqlCommand().ExecuteNonQuery();
+            qry.Disconnect();
+        }
+    }
+}
