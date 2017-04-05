2bca225a7dd56d2b552d6bd855746b50f39d5a2b
Matt Smith <mjsmith92@outlook.com>
2017-04-05 14:51:32 -0400

    Week 13: Autogen stuff that comes with a model.

---
+++ Week13/Models/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Week13.Models
+{
+    public class Student
+    {
+        public int studentID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public double gpa { get; set; }
+    }
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
