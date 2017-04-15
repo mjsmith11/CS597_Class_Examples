77e378e7d9081ecd9311d04bcd743757a3c805db
Matt Smith <mjsmith92@outlook.com>
2017-04-15 13:29:05 -0400

    Creating a MVC stuff based on a DB

---
+++ Week14Friday/Controllers/SchedulesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Week14Friday.Models;
+
+namespace Week14Friday.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        private CoursesEntities db = new CoursesEntities();
+
+        // GET: Schedules
+        public ActionResult Index(string num)
+        {
+            var courseNumberList = new List<string>();
+            var number = from s in db.Schedules select s.CourseNumber;
+            courseNumberList.AddRange(number.Distinct());
+            ViewBag.courseNumber = new SelectList(courseNumberList, 120);//120 is default
+
+            //Response.Write("Hello World"); //this would work
+
+            var courses = from e in db.Schedules select e;
+
+            if(!String.IsNullOrEmpty(num))
+            {
+                courses = courses.Where(e => e.CourseNumber == num);
+            }
+
+            return View(courses.ToList());
+        }
+
+        // GET: Schedules/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedules.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // GET: Schedules/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schedules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CRN,CourseNumber,SectionNumber,Days,StartTime,EndTime")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Schedules.Add(schedule);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(schedule);
+        }
+
+        // GET: Schedules/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedules.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // POST: Schedules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CRN,CourseNumber,SectionNumber,Days,StartTime,EndTime")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(schedule).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(schedule);
+        }
+
+        // GET: Schedules/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedules.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // POST: Schedules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Schedule schedule = db.Schedules.Find(id);
+            db.Schedules.Remove(schedule);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
